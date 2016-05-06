22c193decf8054f741e06c366b2ef8df46337bf2
Qian Huanping <qian.huanping@outlook.com>
2016-05-06 18:11:01 +0000

    commit

--- webcore/src/com/huawei/gnoc/invas/webcore/model/IUser.java
+++ webcore/src/com/huawei/gnoc/invas/webcore/model/IUser.java
@@ -3,7 +3,14 @@ package com.huawei.gnoc.invas.webcore.model;
 import java.util.Date;
 
 public interface IUser {
-	public static final String USER_ID = "userId";
+	public static final String USER_ID = "USER_ID";
+	public static final String USER_NAME = "USER_NAME";
+	public static final String PASSWORD = "USER_PWD";
+	public static final String PWD_ERR_CNT = "PWD_ERR_CNT";
+	public static final String LOCK_FLAG = "LOCK_FLAG";
+	public static final String CREATE_TIME = "CREATE_TIME";
+	public static final String MODIFY_TIME = "MODIFY_TIME";
+	public static final String LAST_LOGIN_TIME = "LAST_LOGIN_TIME";
 	
 	public long getUserId();
 	public String getUserName();
